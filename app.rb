b5813ce11ab0f14cebdfd6a5a993f0dcd553bfc4
Tristan Harris <tris.dev@pumpcottage.me.uk>
2018-09-26 22:19:52 +0100

    Simple web frontend

---
+++ app.rb
@@ -0,0 +1,44 @@
+require 'sinatra/base'
+require_relative 'lib/x32show'
+require 'zip'
+
+class App < Sinatra::Application
+  enable :inline_templates
+
+  get '/' do
+    erb :index
+  end
+
+  post '/convert' do
+    redirect to('/') unless params[:csv]
+    show = X32Show::Show.new('show')
+    show.load_csv(params[:csv][:tempfile].path)
+    file = Tempfile.new('zip')
+    Dir.mktmpdir do |dir|
+      show.save(dir)
+      Zip::File.open(file.path, Zip::File::CREATE) do |zipfile|
+        Dir[dir+'/*/*'].each do |filename|
+          zipfile.add(File.join(show.name, File.basename(filename)), filename)
+        end
+      end
+    end
+    send_file file.path, type: :zip, disposition: :attachment, filename: show.name+'.zip'
+  end
+
+end
+
+__END__
+
+@@ index
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>X32 Plot Converter</title>
+  </head>
+  <body>
+    <form method='post' action='convert' enctype='multipart/form-data'>
+      <input type='file' name='csv' accept='.csv'>
+      <input type='submit'>
+    </form>
+  </body>
+</html>
